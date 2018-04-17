d8c4a92889cbed7791727908571b2937ecdb6a20
f <f@rawtx.com>
2018-04-16 17:25:13 -0700

    react-native-camera seems to be working.
    Former-commit-id: 2bd742cf27fce4747e1020162bfa90b9087dc224

--- rtxwallet/android/app/src/main/java/com/rtxwallet/MainApplication.java
+++ rtxwallet/android/app/src/main/java/com/rtxwallet/MainApplication.java
@@ -8,6 +8,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import org.reactnative.camera.RNCameraPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
           new RtxPackage()
       );
     }
