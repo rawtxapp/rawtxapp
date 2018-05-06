d06a2745fa950553a3f630b1ca6cacb2cf0446ac
f <f@rawtx.com>
2018-05-06 09:22:14 -0700

    Select screen peer prints alias and last updated + some style changes.
    Former-commit-id: f02610c1d84a98a4d26cfebc1af4264893e8abc6

--- rtxwallet/ScreenSelectPeer.js
+++ rtxwallet/ScreenSelectPeer.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { FlatList, StyleSheet, View, Text } from "react-native";
 import withLnd from "./withLnd.js";
 import Button from "react-native-button";
 import shared from "./SharedStyles.js";
+import { RenderNodeInfoItem } from "./ComponentShared";
 
 class ScreenSelectPeer extends Component {
   constructor(props) {
     super(props);
-    this.state = { peers: [] };
+    this.state = {};
   }
 
   componentDidMount() {
@@ -17,28 +18,80 @@ class ScreenSelectPeer extends Component {
   getPeers = async () => {
     try {
       const { peers } = await this.props.lndApi.peers();
-      this.setState({ peers });
+      const nodeInfoPromises = peers.map(p =>
+        this.props.lndApi.getNodeInfo(p.pub_key)
+      );
+      const nodeInfos = (await Promise.all(nodeInfoPromises)).map(
+        ni => ni.node
+      );
+      this.setState({ nodeInfos });
     } catch (err) {}
+  };
+
+  _keyExtractor = (i, ix) => i.pub_key;
+
+  _renderGraphNode = ({ item: n }) => {
+    const lastUpdate = new Date();
+    lastUpdate.setTime(parseInt(n.last_update) * 1000);
+
+    return (
+      <View style={styles.nodeItem}>
+        <Text>
+          <Text style={shared.boldText}>alias:</Text>
+          {n.alias}
+        </Text>
+        <Text>
+          <Text style={shared.boldText}>pubkey:</Text>
+          {n.pub_key}
+        </Text>
+        <Text>
+          <Text style={shared.boldText}>last updated:</Text>
+          {lastUpdate.toDateString() || "No last_update found for node."}
+        </Text>
+        <Button
+          style={[shared.smallButton, shared.textAlignLeft]}
+          onPress={() => this.props.selectPeer(n)}
+        >
+          Open channel to peer
+        </Button>
+      </View>
+    );
   };
 
   render() {
     return (
-      <View style={shared.container}>
-        {this.state.peers &&
-          this.state.peers.map((p, i) => (
-            <View key={i}>
-              <Button onPress={() => this.props.selectPeer(p)}>
-                {p.pub_key}
-              </Button>
-              <View style={shared.separator} />
-            </View>
-          ))}
-        <Button onPress={this.props.onCancel} style={shared.cancelButton}>
-          Cancel
-        </Button>
+      <View style={shared.flexOne}>
+        <View style={styles.scrollContainer}>
+          <FlatList
+            data={this.state.nodeInfos}
+            renderItem={this._renderGraphNode}
+            keyExtractor={this._keyExtractor}
+          />
+        </View>
+        <View style={[styles.actionContainer, shared.centerPrimaryAxis]}>
+          <Button onPress={this.props.onCancel} style={shared.cancelButton}>
+            Cancel
+          </Button>
+        </View>
       </View>
     );
   }
 }
 
 export default withLnd(ScreenSelectPeer);
+
+const styles = StyleSheet.create({
+  nodeItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: "#BDBDBD"
+  },
+  scrollContainer: {
+    flex: 9
+  },
+  actionContainer: {
+    flex: 1,
+    borderTopWidth: 1,
+    borderColor: "gray"
+  }
+});
