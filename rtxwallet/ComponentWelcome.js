f5f6493f98350a42468578e2990b5d28186e24d1
f <f@rawtx.com>
2018-05-19 02:27:01 -0700

    Shorten welcome message.
    Former-commit-id: 64fac000a18b0b985dd6db624bcf04758a4d3d90

--- rtxwallet/ComponentWelcome.js
+++ rtxwallet/ComponentWelcome.js
@@ -30,25 +30,12 @@ export default class ComponentWelcome extends Component {
           Welcome to rawtx lightning wallet!
         </Text>
         <Text>
-          You can send/receive testnet Bitcoins on this app (on the blockchain
-          and lightning network).
-        </Text>
-        <Text>
           Please give the app <Text style={shared.boldText}>15-30 mins</Text> to
           sync and bootstrap, you can come back to check it in a while, it will
           work even if you put it in background!
         </Text>
         <Text>
-          Operations below won't work until you see a{" "}
-          <Text style={shared.boldText}>"Synced"</Text> message in green above.
-        </Text>
-        <Text>
-          If you run into problems, you can find us on twitter or reddit under
-          rawtxapp usernames. Our email is rawtxapp@gmail.com.
-        </Text>
-        <Text>
-          Please show what lightning is capable of to your friends and family
-          and leave us a review on the app store!
+          Please show what lightning is capable of to your friends and family!
         </Text>
         <Button
           style={[shared.inCardButton]}
