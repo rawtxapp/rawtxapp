1694fddc7a0fd85bf269b69aaf93c09c5affa8fc
f <f@rawtx.com>
2018-06-09 16:49:50 -0700

    Write iOS specific welcome message.

--- ComponentWelcome.js
+++ ComponentWelcome.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { AsyncStorage, StyleSheet, Text, View } from "react-native";
+import { AsyncStorage, Platform, StyleSheet, Text, View } from "react-native";
 import { styles as theme } from "react-native-theme";
 import Button from "react-native-button";
 
@@ -22,6 +22,33 @@ export default class ComponentWelcome extends Component {
     } catch (e) {}
   };
 
+  _renderAndroidMessage = () => {
+    return (
+      <View>
+        <Text>
+          Please give the app <Text style={theme.boldText}>15-30 mins</Text> to
+          sync and bootstrap, you can come back to check it in a while, it will
+          work even if you put it in background!
+        </Text>
+      </View>
+    );
+  };
+
+  _renderiOSMessage = () => {
+    return (
+      <View>
+        <Text>
+          It should take about 15-30 mins to sync with the blockchain after
+          which you can start opening channels.
+        </Text>
+        <Text>
+          You can close the app whenever you want, it will continue syncing from
+          where it left when you reopen.
+        </Text>
+      </View>
+    );
+  };
+
   render() {
     if (this.state.showed) return <View />;
     return (
@@ -29,11 +56,9 @@ export default class ComponentWelcome extends Component {
         <Text style={theme.accountHeader}>
           Welcome to rawtx lightning wallet!
         </Text>
-        <Text>
-          Please give the app <Text style={theme.boldText}>15-30 mins</Text> to
-          sync and bootstrap, you can come back to check it in a while, it will
-          work even if you put it in background!
-        </Text>
+        {Platform.OS == "ios"
+          ? this._renderiOSMessage()
+          : this._renderAndroidMessage()}
         <Text>
           Please show what lightning is capable of to your friends and family!
         </Text>
