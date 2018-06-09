51d321a42da5c1f230363c1fab2178e7956f0061
f <f@rawtx.com>
2018-06-09 01:49:30 -0700

    Remove dead code in actionsheet.

--- ComponentActionSheet.js
+++ ComponentActionSheet.js
@@ -21,60 +21,6 @@ import { styles as theme } from "react-native-theme";
 
 var window = Dimensions.get("window");
 
-class FadeInView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fadeAnim: new Animated.Value(0)
-    };
-  }
-
-  componentDidMount() {
-    this._animate(this.props);
-  }
-
-  componentWillReceiveProps(newProps) {
-    this._animate(newProps);
-  }
-
-  _animate(newProps) {
-    return Animated.timing(this.state.fadeAnim, {
-      toValue: newProps.visible ? 0.7 : 0,
-      duration: 300
-    }).start();
-  }
-
-  render() {
-    return (
-      <Animated.View
-        style={[
-          fadeInStyles.overlay,
-          this.props.visible && fadeInStyles.visible,
-          { opacity: this.state.fadeAnim },
-          { backgroundColor: this.props.backgroundColor || "black" }
-        ]}
-      >
-        {this.props.children}
-      </Animated.View>
-    );
-  }
-}
-
-const fadeInStyles = StyleSheet.create({
-  overlay: {
-    top: window.height,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    height: window.height,
-    width: window.width,
-    position: "absolute"
-  },
-  visible: {
-    top: 0
-  }
-});
-
 class ActionModal extends Component {
   componentWillReceiveProps(newProps) {
     if (newProps.visible) {
