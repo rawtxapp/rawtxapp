1daf92d36d8b28c6f4c26875e6de24614178a9fb
f <f@rawtx.com>
2018-07-30 15:13:35 -0700

    screenpayments incorporates invoices.

--- src/ScreenPayments.js
+++ src/ScreenPayments.js
@@ -3,6 +3,7 @@ import {
   ActivityIndicator,
   FlatList,
   LayoutAnimation,
+  SectionList,
   ScrollView,
   StyleSheet,
   Text,
@@ -16,28 +17,89 @@ import {
   orderNodesByLastUpdate,
   updateNodesInAndOutCounts,
   orderNodesByRtxScore,
-  sortPaymentsByCreationDateDescending
+  sortPaymentsByCreationDateDescending,
+  sortTransactions
 } from "./Utils";
 import withTheme from "./withTheme";
 
 class ScreenPayments extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    // transactions is formatted [{title:'<DATE>', data:[<PAYMENTS_INVOICES>]}]
+    this.state = { transactions: [] };
   }
 
   componentDidMount() {
     this.getPayments();
+    this.getInvoices();
   }
+
+  setTransactions = () => {
+    const txs = this.state.transactions || {};
+    payments = this.state.payments;
+    invoices = this.state.invoices;
+
+    if (!payments || !invoices) return;
+
+    // SectionList takes [{dateKey,value},{dateKey,value},...], but we don't
+    // want to search the list for every dateKey insertion, so keep track of
+    // indexes.
+    const dateToIx = {};
+    for (let i = 0; i < txs.length; i++) {
+      dateToIx[txs[i].title] = i;
+    }
+
+    const pushToDate = (obj, time) => {
+      const d = new Date(parseInt(time) * 1000);
+      const date = d.toDateString();
+
+      if (date in dateToIx) {
+        const ix = dateToIx[date];
+        txs[ix].data.push(obj);
+      } else {
+        const newLength = txs.push({ title: date, data: [obj] });
+        dateToIx[date] = newLength - 1;
+      }
+    };
+
+    for (let i = 0; i < payments.length; i++) {
+      pushToDate(payments[i], payments[i].creation_date);
+    }
+
+    for (let i = 0; i < invoices.length; i++) {
+      pushToDate(invoices[i], invoices[i].settle_date);
+    }
+
+    sortTransactions(txs);
+    this.setState({ transactions: txs });
+  };
 
   getPayments = async () => {
     try {
       const { payments } = await this.props.lndApi.getPayments();
-      sortPaymentsByCreationDateDescending(payments);
-      LayoutAnimation.easeInEaseOut();
-      this.setState({ payments: payments || [] });
+      this.setState({ payments }, () => {
+        this.setTransactions();
+      });
     } catch (err) {
       this.setState({ payments: [] });
+    }
+  };
+
+  getInvoices = async () => {
+    try {
+      const { invoices } = await this.props.lndApi.getInvoices();
+      const settledInvoices = [];
+
+      for (let i = 0; i < invoices.length; i++) {
+        if (invoices[i].settled) {
+          settledInvoices.push(invoices[i]);
+        }
+      }
+      this.setState({ invoices: settledInvoices }, () => {
+        this.setTransactions();
+      });
+    } catch (err) {
+      this.setState({ invoices: [] });
     }
   };
 
@@ -46,34 +108,40 @@ class ScreenPayments extends Component {
     creationDate.setTime(parseInt(n.creation_date) * 1000);
 
     return (
-      <View style={[styles.nodeItem, this.props.theme.actionContainer]}>
-        <Text selectable style={this.props.theme.actionContainerText}>
-          <Text style={theme.boldText}>
-            Paid {this.props.displaySatoshi(n.value)}
+      <View style={[styles.nodeItem]}>
+        <View style={styles.leftContainer}>
+          <Text selectable>{n.memo}</Text>
+          <Text selectable>{n.payment_hash}</Text>
+          <Text selectable style={theme.boldText}>
+            {creationDate.toTimeString().substr(0, 5) ||
+              "No creation date found."}
           </Text>
-        </Text>
-        <Text selectable style={this.props.theme.actionContainerText}>
-          <Text style={theme.boldText}>on </Text>
-          {creationDate.toDateString() + " " + creationDate.toTimeString() ||
-            "No creation date found."}
-        </Text>
-        <Text style={this.props.theme.actionContainerText}>
-          <Text style={theme.boldText}>fee:</Text>
-          {n.fee || 0}
-        </Text>
-        <Text style={this.props.theme.actionContainerText}>
-          <Text style={theme.boldText}>path length:</Text>
-          {n.path.length}
-        </Text>
-        <Text selectable style={this.props.theme.actionContainerText}>
-          <Text style={theme.boldText}>payment hash:</Text>
-          {n.payment_hash}
-        </Text>
+        </View>
+        <View style={styles.rightContainer}>
+          <View
+            style={
+              n.settled
+                ? theme.channelStatusActiveContainer
+                : theme.channelStatusInactiveContainer
+            }
+          >
+            <Text
+              selectable
+              style={
+                n.settled
+                  ? theme.successTextColorOnly
+                  : theme.errorTextColorOnly
+              }
+            >
+              {this.props.displaySatoshi(n.value)}
+            </Text>
+          </View>
+        </View>
       </View>
     );
   };
 
-  _keyExtractor = (i, ix) => i.payment_hash;
+  _keyExtractor = (i, ix) => i.payment_hash || i.r_hash;
 
   _renderFlatList = () => {
     if (!this.state.payments) return <ActivityIndicator />;
@@ -90,10 +158,28 @@ class ScreenPayments extends Component {
     );
   };
 
+  _renderSectionList = () => {
+    if (!this.state.transactions) return <ActivityIndicator />;
+    return (
+      <SectionList
+        sections={this.state.transactions}
+        renderItem={this._renderPayment}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.sectionHeader}>{title}</Text>
+        )}
+        keyExtractor={this._keyExtractor}
+        ListEmptyComponent={<Text>There are no transactions.</Text>}
+        SectionSeparatorComponent={({ trailingItem }) =>
+          trailingItem ? null : <View style={styles.separator} />
+        }
+      />
+    );
+  };
+
   render() {
     return (
       <View>
-        <View style={styles.scrollContainer}>{this._renderFlatList()}</View>
+        <View style={styles.scrollContainer}>{this._renderSectionList()}</View>
       </View>
     );
   }
@@ -103,6 +189,22 @@ export default withTheme(withLnd(ScreenPayments));
 
 const styles = StyleSheet.create({
   nodeItem: {
-    padding: 10
+    padding: 10,
+    flexDirection: "row"
+  },
+  sectionHeader: {
+    marginLeft: 10,
+    fontSize: 18
+  },
+  leftContainer: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  rightContainer: {
+    flex: 0,
+    justifyContent: "center"
+  },
+  separator: {
+    marginTop: 30
   }
 });
