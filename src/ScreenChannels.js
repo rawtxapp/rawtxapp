3e0b68431976b91802767e57e0ec30c02a811f29
f <f@rawtx.com>
2018-10-31 17:16:36 -0700

    cleanup ScreenChannels.

--- src/ScreenChannels.js
+++ src/ScreenChannels.js
@@ -33,73 +33,7 @@ class ScreenChannels extends Component {
     }
   };
 
-  _renderChannelItem = (c, ix) => {
-    const errorKey = "error" + c.chan_id;
-    const successKey = "success" + c.chan_id;
-    const closeChannelDisableKey = "disable" + c.chan_id;
-    return (
-      <View key={ix} style={styles.nodeItem}>
-        <Text selectable>
-          <Text style={theme.boldText}>Channel id: </Text>
-          {c.chan_id}
-        </Text>
-
-        <Text selectable>
-          <Text style={theme.boldText}>Capacity: </Text>
-          {c.capacity}
-        </Text>
-
-        <Text selectable>
-          <Text style={theme.boldText}>Local balance: </Text>
-          {c.local_balance}
-        </Text>
-
-        <Text selectable>
-          <Text style={theme.boldText}>Remote balance: </Text>
-          {c.remote_balance}
-        </Text>
-
-        <Text selectable>
-          <Text style={theme.boldText}>Is active: </Text>
-          {c.active ? "yes" : "no"}
-        </Text>
-
-        <Button
-          style={[theme.smallButton, theme.textAlignLeft]}
-          disabled={this.state[closeChannelDisableKey]}
-          styleDisabled={theme.disabledButton}
-          onPress={async () => {
-            this.setState({ [closeChannelDisableKey]: true });
-            try {
-              const result = await this.props.lndApi.closeChannel(
-                c.channel_point
-              );
-              this.setState({ [successKey]: "Request for closure sent!" });
-            } catch (err) {
-              if (err == "timeout") {
-                this.setState({
-                  [errorKey]:
-                    "Received a timeout, but it's possible channel is being closed in the background."
-                });
-              } else {
-                this.setState({ [errorKey]: JSON.stringify(err.message) });
-              }
-            }
-          }}
-        >
-          Close channel
-        </Button>
-        {this.state[errorKey] && (
-          <Text style={theme.errorText}>{this.state[errorKey]}</Text>
-        )}
-        {this.state[successKey] && (
-          <Text style={theme.successText}>{this.state[successKey]}</Text>
-        )}
-      </View>
-    );
-  };
-
-  _keyExtractor = (c, ix) => c.chan_id;
+  _keyExtractor = (c, ix) => c.channel_point;
 
   render() {
     return (
