7b7327a077a49daac3c3891f3e352ac4da039e74
f <f@rawtx.com>
2019-04-07 21:32:39 -0700

    using AsyncStorage from community in micro.

--- src/micro/Micro.js
+++ src/micro/Micro.js
@@ -1,5 +1,5 @@
 /* @flow */
-import { AsyncStorage } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
 import typeof EventEmitter from "EventEmitter";
 
 // For initial release, only allow up to 10k sat
